e85fbcba555513beedce18cbaff2fe6874530858
imxiaoxixi <15127129151@163.com>
2022-10-06 10:30:36 +0800

    人力资源项目初始化-肃清模板后

---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, 
+  timeout: 5000
+})
+
+service.interceptors.request.use(
+  config => { 
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+  return response
+      },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+export default service
